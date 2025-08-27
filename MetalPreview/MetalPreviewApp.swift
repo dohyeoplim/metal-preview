7bf9435388fc8a5a35757df254a09cbd39df1e26
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-28 08:09:53 +0900

    Initial Commit

---
+++ MetalPreview/MetalPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPreviewApp.swift
+//  MetalPreview
+//
+//  Created by dohyeoplim on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
